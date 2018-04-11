67e2e85ae62736b82b9c76f0856bc66e457ab52a
jaanhio <jianhaot.89@gmail.com>
2018-04-11 10:05:28 +0800

    done

--- word.rb
+++ word.rb
@@ -1,6 +1,15 @@
 # write your solution here
 
-test_string = "rhythm"
+test_string1 = "egg"
+test_string2 = "inbox"
+test_string3 = "eight"
+test_string4 = "happy"
+test_string5 = "duck"
+test_string6 = "glove"
+test_string7 = "yellow"
+test_string8 = "rhythm"
+test_string9 = "queen"
+test_string10 = "squeal"
 
 def pig_latin_translate(str)
   # generate array for all alphabets
@@ -11,13 +20,28 @@ def pig_latin_translate(str)
 
   if vowels.include?(str[0])
     str + 'ay'
+  elsif consonants.include?(str[0]) && consonants.include?(str[1]) && vowels.include?(str[2])
+    str[3..-1] + str[0..2] + 'ay'
   elsif consonants.include?(str[0]) && consonants.include?(str[1])
+    str[2..-1] + str[0..1] + 'ay'
+  elsif consonants.include?(str[0]) && vowels.include?(str[1])
     str[2..-1] + str[0..1] + 'ay'
   elsif consonants.include?(str[0])
     str[1..-1] + str[0] + 'ay'
+  # elsif consonants.include?(str[0]) && consonants.include?(str[1]) && vowels.include?(str[2])
+  #   str[3..-1] + str[0..2] + 'ay'
   else
     str # return unchanged
   end
 end
 
-puts pig_latin_translate(test_string)
+puts pig_latin_translate(test_string1)
+puts pig_latin_translate(test_string2)
+puts pig_latin_translate(test_string3)
+puts pig_latin_translate(test_string4)
+puts pig_latin_translate(test_string5)
+puts pig_latin_translate(test_string6)
+puts pig_latin_translate(test_string7)
+puts pig_latin_translate(test_string8)
+puts pig_latin_translate(test_string9)
+puts pig_latin_translate(test_string10)
